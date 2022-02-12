357a07b8124e16dee373bf60088b946efa2926c3
GalDavid6 <76738300+GalDavid6@users.noreply.github.com>
2022-02-12 16:04:27 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SimpleSnakeP
+ 
---
+++ SimpleSnakeP.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+bool GameOver;
+
+// Map Size
+const int  width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+
+void Setup()
+{
+    GameOver = false;
+}
+
+void Draw()
+{
+
+}
+
+void Input()
+{
+
+}
+
+void Logic()
+{
+
+}
+
+int main()
+{
+    Setup();
+    while (!GameOver)
+    {
+        Draw();
+        Input();
+        Logic();
+
+    }
+
+}
